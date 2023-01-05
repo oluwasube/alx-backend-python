6dc980686acb3ac6c62917972f3d40a879bd184a
oluwasube <enibeke20@gmail.com>
2023-01-05 10:40:22 +0100

    task 5 success

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-def sum_list(input_list:list[float]) ->float:
+from typing import List
+def sum_list(input_list:List[float]) ->float:
     total = 0
     for num in input_list:
         total += num
