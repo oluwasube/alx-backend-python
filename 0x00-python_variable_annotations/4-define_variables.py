750b3817ba6099211c3b076b7be1c89d153a5b0f
oluwasube <enibeke20@gmail.com>
2023-01-05 09:49:18 +0100

    task 4 success

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+a = 1 # a is an integer with a value of 1
+pi = 3.14 # pi is a float with a value of 3.14
+i_understand_annotations = True # i_understand_annotations is a boolean with a value of True
+school = "Holberton" # school is a string with a value of "Holberton"
